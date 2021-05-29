68605125c22211e46ec983a2e3e23e5becef025c
seina25 <replace421@gmail.com>
2021-05-29 07:42:59 +0000

    [Add]Rubocop

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -7,22 +7,19 @@ class PostImagesController < ApplicationController
     @post_image = PostImage.new(post_image_params)
     @post_image.user_id = current_user.id
     if @post_image.save
-     redirect_to post_images_path
+      redirect_to post_images_path
     else
-     render :new
+      render :new
     end
-
   end
 
   def index
     @post_images = PostImage.page(params[:page]).reverse_order
-
   end
 
   def show
     @post_image = PostImage.find(params[:id])
-    @post_comment =PostComment.new
-
+    @post_comment = PostComment.new
   end
 
   def destroy
@@ -32,6 +29,7 @@ class PostImagesController < ApplicationController
   end
 
   private
+
   def post_image_params
     params.require(:post_image).permit(:shop_name, :image, :caption)
   end
